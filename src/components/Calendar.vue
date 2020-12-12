b6f4a834fe069e7986e0617a409a6645f93208e2
Sayaki-M <sayaki3891.qwxzponm@gmail.com>
2020-12-12 19:21:56 +0900

    add カレンダー埋め込み

---
+++ src/components/Calendar.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <iframe src="https://calendar.google.com/calendar/u/0/embed?src=jackapp.tmy@gmail.com&ctz=Asia/Tokyo" showTitle=0 showTabs = 0 showPrint=0></iframe>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+iframe {
+  width:100%;
+  max-width: 800px;
+  height: 400px;
+}
+</style>
